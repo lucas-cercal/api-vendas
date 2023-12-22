673da256db054aab1f70515dac91644fe7db8969
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-22 09:53:26 -0300

    feat: add typeorm configuration

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -27,5 +27,5 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log('Server started on port 3333! 🏆');
 });
