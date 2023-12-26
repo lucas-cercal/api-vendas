c727cc35d869cfa67630379f114c3a97cc55ce2d
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-26 15:32:15 -0300

    feat: add product routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((error: Error, req: Request, res: Response) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: 'error',
