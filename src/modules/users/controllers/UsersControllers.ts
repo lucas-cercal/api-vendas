d71ab6e51d6d2e9d994038ddb91adc8216bc01a1
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-29 11:43:20 -0300

    feat: add users controller

---
+++ src/modules/users/controllers/UsersControllers.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import ListUserService from '../services/ListUserService';
+import CreateUserService from '../services/CreateUserService';
+
+export default class UsersControllers {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = listUser.execute();
+
+    return res.json(users);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    return res.json(user);
+  }
+}
