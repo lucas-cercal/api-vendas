3122e73708517049cd8736b7858bd9149c1734bb
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-02 10:05:24 -0300

    feat: add express request object override

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   req: Request,
   res: Response,
@@ -15,7 +21,13 @@ export default function isAuthenticated(
   const token = authHeader.split(' ')[1];
 
   try {
-    verify(token, authConfig.jwt.secret);
+    const decodedToken = verify(token, authConfig.jwt.secret);
+    const { sub } = decodedToken as TokenPayload;
+
+    req.user = {
+      id: sub,
+    };
+
     return next();
   } catch (error) {
     throw new AppError('Invalid JWT Token.');
