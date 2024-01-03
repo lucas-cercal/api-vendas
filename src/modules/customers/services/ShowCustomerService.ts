af44e838cd6947d342de5ecd4eac0ccc0e17409c
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-03 17:42:50 -0300

    feat: add customers services

---
+++ src/modules/customers/services/ShowCustomerService.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+import Customer from '../typeorm/entities/Customer';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowCustomerService {
+  public async execute({ id }: IRequest): Promise<Customer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const customer = await customersRepository.findById(id);
+
+    if (!customer) throw new AppError('Customer not found.');
+
+    return customer;
+  }
+}
+
+export default ShowCustomerService;
