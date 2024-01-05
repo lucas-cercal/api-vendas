02ea160daee72cbef79d5a52100caf8b18d08bce
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-05 17:34:26 -0300

    feat: add redis class

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,11 +1,16 @@
 import { getCustomRepository } from 'typeorm';
 import ProductsRepository from '../typeorm/repositories/ProductsRepository';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductsRepository);
+    const redisCache = new RedisCache();
+
     const products = await productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
 
     return products;
   }
