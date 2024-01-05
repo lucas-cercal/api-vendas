8eac4ea35d60b2787a7e9de6a077eb7dc4ff13a2
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-05 11:17:37 -0300

    feat: add pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,12 +2,23 @@ import { getCustomRepository } from 'typeorm';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 import Customer from '../typeorm/entities/Customer';
 
-class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
-    const customersRepository = getCustomRepository(CustomersRepository);
-    const customers = await customersRepository.find();
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
 
-    return customers;
+class ListCustomerService {
+  public async execute(): Promise<IPaginateCustomer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const customers = await customersRepository.createQueryBuilder().paginate();
+
+    return customers as IPaginateCustomer;
   }
 }
 
