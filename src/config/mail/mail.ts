bf3a1db71057ef8bdde5b69d6ede909151e4377a
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-08 14:42:24 -0300

    feat: add ses - part 01

---
+++ src/config/mail/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'contato.apivendas@cercaldev.com',
+      name: 'Lucas Cercal',
+    },
+  },
+} as IMailConfig;
