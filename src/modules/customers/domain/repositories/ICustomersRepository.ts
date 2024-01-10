66430c7a24c702165d98efaa16b720f300f5a42d
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-10 17:48:30 -0300

    feat: add ICustomersRepository

--- src/modules/customers/domain/repositories/ICustomersRepository.ts
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -1,9 +1,10 @@
+import { ICreateCustomer } from '../models/ICreateCustomer';
 import { ICustomer } from '../models/ICustomer';
 
 export default interface ICustomersRepository {
   findByName(name: string): Promise<ICustomer | undefined>;
   findById(id: string): Promise<ICustomer | undefined>;
   findByEmail(email: string): Promise<ICustomer | undefined>;
-  // create(data: ICreateCustomer): Promise<ICustomer>;
-  // save(customer: ICustomer): Promise<ICustomer>;
+  create(data: ICreateCustomer): Promise<ICustomer>;
+  save(customer: ICustomer): Promise<ICustomer>;
 }
--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,14 +1,30 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import Customer from '../entities/Customer';
 import ICustomersRepository from '@modules/customers/domain/repositories/ICustomersRepository';
+import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer';
 
-@EntityRepository(Customer)
-export default class CustomersRepository
-  extends Repository<Customer>
-  implements ICustomersRepository
-{
+export default class CustomersRepository implements ICustomersRepository {
+  private ormRepository: Repository<Customer>;
+
+  constructor() {
+    this.ormRepository = getRepository(Customer);
+  }
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = this.ormRepository.create({ name, email });
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
   public async findByName(name: string): Promise<Customer | undefined> {
-    const customer = this.findOne({
+    const customer = this.ormRepository.findOne({
       where: {
         name,
       },
@@ -18,7 +34,7 @@ export default class CustomersRepository
   }
 
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = this.findOne({
+    const customer = this.ormRepository.findOne({
       where: {
         id,
       },
@@ -28,7 +44,7 @@ export default class CustomersRepository
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = this.findOne({
+    const customer = this.ormRepository.findOne({
       where: {
         email,
       },
