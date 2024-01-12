348a8eeb1c7b215bb03c63f1d7ef66c6f74aeb06
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-12 10:55:09 -0300

    refactor: refactoring ProductsController

--- src/modules/products/infra/http/controllers/ProductsController.ts
+++ src/modules/products/infra/http/controllers/ProductsController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
-import ListProductService from '../../../services/ListProductService';
-import ShowProductService from '../../../services/ShowProductService';
-import CreateProductService from '../../../services/CreateProductService';
-import UpdateProductService from '../../../services/UpdateProductService';
-import DeleteProductService from '../../../services/DeleteProductService';
+import { container } from 'tsyringe';
+import CreateProductService from '@modules/products/services/CreateProductService';
+import DeleteProductService from '@modules/products/services/DeleteProductService';
+import ListProductService from '@modules/products/services/ListProductService';
+import ShowProductService from '@modules/products/services/ShowProductService';
+import UpdateProductService from '@modules/products/services/UpdateProductService';
 
 export default class ProductsController {
   public async index(req: Request, res: Response): Promise<Response> {
-    const listProducts = new ListProductService();
+    const listProducts = container.resolve(ListProductService);
 
     const products = await listProducts.execute();
 
@@ -17,7 +18,7 @@ export default class ProductsController {
   public async show(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
 
-    const showProduct = new ShowProductService();
+    const showProduct = container.resolve(ShowProductService);
 
     const product = await showProduct.execute({ id });
 
@@ -27,7 +28,7 @@ export default class ProductsController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { name, price, quantity } = req.body;
 
-    const createProduct = new CreateProductService();
+    const createProduct = container.resolve(CreateProductService);
 
     const product = await createProduct.execute({ name, price, quantity });
 
@@ -38,7 +39,7 @@ export default class ProductsController {
     const { name, price, quantity } = req.body;
     const { id } = req.params;
 
-    const updateProduct = new UpdateProductService();
+    const updateProduct = container.resolve(UpdateProductService);
 
     const product = await updateProduct.execute({ id, name, price, quantity });
 
@@ -48,7 +49,7 @@ export default class ProductsController {
   public async delete(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
 
-    const deleteProduct = new DeleteProductService();
+    const deleteProduct = container.resolve(DeleteProductService);
 
     await deleteProduct.execute({ id });
 
