3708c934d9de1d17b35bd0d21b358908d55d5131
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-12 15:40:05 -0300

    feat: add UserTokensRepository

--- src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -1,10 +1,16 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { IUserTokensRepository } from '@modules/users/domain/repositories/IUserTokensRepository';
+import { getRepository, Repository } from 'typeorm';
 import UserToken from '../entities/UserToken';
 
-@EntityRepository(UserToken)
-export default class UserTokensRepository extends Repository<UserToken> {
+export default class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
   public async findByToken(token: string): Promise<UserToken | undefined> {
-    const userToken = this.findOne({
+    const userToken = this.ormRepository.findOne({
       where: {
         token,
       },
@@ -14,11 +20,11 @@ export default class UserTokensRepository extends Repository<UserToken> {
   }
 
   public async generate(user_id: string): Promise<UserToken> {
-    const userToken = this.create({
+    const userToken = this.ormRepository.create({
       user_id,
     });
 
-    await this.save(userToken);
+    await this.ormRepository.save(userToken);
 
     return userToken;
   }
