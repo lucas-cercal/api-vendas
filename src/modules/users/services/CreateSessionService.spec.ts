70743191b72a64492c09656f5c1806218575746e
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-17 11:47:43 -0300

    feat: add CreateSessionService.spec

---
+++ src/modules/users/services/CreateSessionService.spec.ts
@@ -0,0 +1,60 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../domain/repositories/fakes/FakeUsersRepository';
+import CreateSessionsService from './CreateSessionsService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let createSession: CreateSessionsService;
+let hashProvider: FakeHashProvider;
+
+describe('Create session', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    hashProvider = new FakeHashProvider();
+    createSession = new CreateSessionsService(
+      fakeUsersRepository,
+      hashProvider,
+    );
+  });
+
+  it('Should be able to authenticate', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Lucas Cercal',
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    const response = await createSession.execute({
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+
+  it('Should not be able to authenticate with non existent user', async () => {
+    expect(
+      createSession.execute({
+        email: 'teste@teste.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to authenticate with wrong password', async () => {
+    await fakeUsersRepository.create({
+      name: 'Lucas Cercal',
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    expect(
+      createSession.execute({
+        email: 'teste@teste.com',
+        password: '654321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
