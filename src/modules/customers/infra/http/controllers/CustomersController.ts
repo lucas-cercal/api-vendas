b650d33c60ffff1d7180b9f65363175ad338ecf5
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-18 11:54:52 -0300

    feat: add ICustomerPaginate

--- src/modules/customers/infra/http/controllers/CustomersController.ts
+++ src/modules/customers/infra/http/controllers/CustomersController.ts
@@ -1,59 +1,67 @@
 import { Request, Response } from 'express';
-import ListCustomerService from '../../../services/ListCustomerService';
-import ShowCustomerService from '../../../services/ShowCustomerService';
-import CreateCustomerService from '../../../services/CreateCustomerService';
-import UpdateCustomerService from '../../../services/UpdateCustomerService';
-import DeleteCustomerService from '../../../services/DeleteCustomerService';
-import CustomersRepository from '../../typeorm/repositories/CustomersRepository';
 import { container } from 'tsyringe';
+import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
+import DeleteCustomerService from '@modules/customers/services/DeleteCustomerService';
+import ListCustomerService from '@modules/customers/services/ListCustomerService';
+import ShowCustomerService from '@modules/customers/services/ShowCustomerService';
+import UpdateCustomerService from '@modules/customers/services/UpdateCustomerService';
 
 export default class CustomersController {
-  public async index(req: Request, res: Response): Promise<Response> {
-    const listCostumers = new ListCustomerService();
+  public async index(request: Request, response: Response): Promise<Response> {
+    const page = request.query.page ? Number(request.query.page) : 1;
+    const limit = request.query.limit ? Number(request.query.limit) : 15;
 
-    const customers = await listCostumers.execute();
+    const listCustomers = container.resolve(ListCustomerService);
+    const customers = await listCustomers.execute({ page, limit });
 
-    return res.json(customers);
+    return response.json(customers);
   }
 
-  public async show(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
 
-    const showCustomer = new ShowCustomerService();
+    const showCustomer = container.resolve(ShowCustomerService);
 
     const customer = await showCustomer.execute({ id });
 
-    return res.json(customer);
+    return response.json(customer);
   }
 
-  public async create(req: Request, res: Response): Promise<Response> {
-    const { name, email } = req.body;
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
 
     const createCustomer = container.resolve(CreateCustomerService);
 
-    const customer = await createCustomer.execute({ name, email });
+    const customer = await createCustomer.execute({
+      name,
+      email,
+    });
 
-    return res.json(customer);
+    return response.json(customer);
   }
 
-  public async update(req: Request, res: Response): Promise<Response> {
-    const { name, email } = req.body;
-    const { id } = req.params;
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const { id } = request.params;
 
-    const updateCustomer = new UpdateCustomerService();
+    const updateCustomer = container.resolve(UpdateCustomerService);
 
-    const customer = await updateCustomer.execute({ id, name, email });
+    const customer = await updateCustomer.execute({
+      id,
+      name,
+      email,
+    });
 
-    return res.json(customer);
+    return response.json(customer);
   }
 
-  public async delete(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
 
-    const deleteCustomer = new DeleteCustomerService();
+    const deleteCustomer = container.resolve(DeleteCustomerService);
 
     await deleteCustomer.execute({ id });
 
-    return res.json([]);
+    return response.json([]);
   }
 }
--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,15 +1,30 @@
-import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
-import { ICustomer } from '../domain/models/ICustomer';
 import { inject, injectable } from 'tsyringe';
+import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
+import { ICustomerPaginate } from '../domain/models/ICustomerPaginate';
+
+interface SearchParams {
+  page: number;
+  limit: number;
+}
 
 @injectable()
 class ListCustomerService {
   constructor(
-    @inject('ICustomersRepository')
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
-  public async execute(): Promise<ICustomer[] | undefined> {
-    const customers = await this.customersRepository.findAll();
+
+  public async execute({
+    page,
+    limit,
+  }: SearchParams): Promise<ICustomerPaginate> {
+    const take = limit;
+    const skip = (Number(page) - 1) * take;
+    const customers = await this.customersRepository.findAll({
+      page,
+      skip,
+      take,
+    });
 
     return customers;
   }
